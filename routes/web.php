397c1715ec294ac7c744df85d6719e62a5f86f8e
nzm-el <ipmen2047@gmail.com>
2026-03-11 09:59:00 +0700

    Menyelesaikan Praktikum PWL 1-3

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\COntrollers\PhotoController;
+
+// Route::resource('photos', PhotoController::class);
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+
+// Route::get('/user/{nama}', function ($nama) {
+//     return 'Nama saya ' . $nama; 
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID ' . $id;
+// });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// });
